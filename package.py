3bf2739d9e92e48f95556fc861fb7d3aeb85a117
Philippe Leprince <philippe@ynput.io>
2025-05-21 16:08:51 +0200

    Update package.py

--- package.py
+++ package.py
@@ -1,9 +1,9 @@
 # Required: lower case addon name e.g. 'deadline', otherwise addon
 #   will be invalid
-name = ""
+name = "azureboards"
 
 # Optional: Addon title shown in UI, 'name' is used by default e.g. 'Deadline'
-title = ""
+title = "Azure Boards"
 
 # Required: Valid semantic version (https://semver.org/)
 version = "0.0.0"
@@ -13,13 +13,15 @@ version = "0.0.0"
 client_dir = None
 
 # Version compatibility with AYON server
-# ayon_server_version = ">=1.0.7"
+ayon_server_version = ">=1.8.0"
 # Version compatibility with AYON launcher
 # ayon_launcher_version = ">=1.0.2"
 
 # Mapping of addon name to version requirements
 # - addon with specified version range must exist to be able to use this addon
-ayon_required_addons = {}
+ayon_required_addons = {
+  "core": ">=1.0.12",
+}
 # Mapping of addon name to version requirements
 # - if addon is used in same bundle the version range must be valid
 ayon_compatible_addons = {}
